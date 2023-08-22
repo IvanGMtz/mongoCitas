86e81f169b4b37b4ec6a285b74efb429a7a92bf2
IvanGMtz <navi.garces24@gmail.com>
2023-08-22 17:07:11 -0500

    refactor: :fire: Update endpoint usuario

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -1,15 +1,11 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
 import {getUsuarios, addUsuario} from "../controllers/usuario.js"
-import passportHelper from '../helpers/passport.js';
-import {validarPermisos} from "../middlewares/rol.js"
+import { verifyToken } from '../middlewares/JWT.js';
 
 const appUsuario = express.Router();
 
-appUsuario.get("/", passportHelper.authenticate('bearer', { session: false }), validarPermisos, limitGet(), getUsuarios);
-appUsuario.post("/",passportHelper.authenticate('bearer', { session: false }), validarPermisos, limitGet(), addUsuario);
-// appUsuario.get("/:id", limitGet(), getUsuario);
-// appUsuario.put("/:id", limitGet(), updateUsuario);
-// appUsuario.delete("/:id", limitGet(), deleteUsuario);
+appUsuario.get("/", verifyToken, limitGet(), getUsuarios);
+appUsuario.post("/", verifyToken, limitGet(), addUsuario);
 
 export default appUsuario;
